0a949d7334d2befebc6dfcde4af43fcc965c7bec
xbq <894895282@qq.com>
2017-12-29 16:40:14 +0800

    '表单验证以及表单数据的提取'

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,12 +5,28 @@ import { AuthService } from '../core/auth.service';
   selector: 'app-login',
   template: `
     <div>
-      用户名：<input required  type="text"  placeholder="用户名" [(ngModel)]="userName">
-      <br>
+    <form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
+    <fieldset ngModelGroup="login">
+      用户名：<input type="text"  [(ngModel)]="userName" placeholder="用户名" #userNameRef = "ngModel" name="userName"
+      required 
+      minlength=3 
 
-      密&nbsp;&nbsp;&nbsp;&nbsp;码：<input required  type="password" placeholder="密码" [(ngModel)]="password">
+      ng-reflect-minlength="3" 
+      ng-reflect-name="username"
+      >
+      {{userNameRef.errors|json}}
+        <span *ngIf="userNameRef.errors?.required">this is required</span>
+        <span *ngIf="userNameRef.errors?.minlength">shuold be at least 3 charactors</span>
       <br>
-      <button  (click)="Login(userName.value,password.value)">Login</button>
+      密&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" [(ngModel)]="password" placeholder="密码" #passwordRef = "ngModel" name="password"
+      required  
+      minlength=6>
+      <span *ngIf="passwordRef.errors?.required">this is required</span>
+        <span *ngIf="passwordRef.errors?.minlength">shuold be at least 3 charactors</span>
+      <br>
+      </fieldset>
+      <button type="submit">Login</button>
+      </form>
     </div>
   `,
   styles: [],
@@ -22,11 +38,11 @@ export class LoginComponent implements OnInit {
 
   }
 
-  userName = "";
-  password = "";
-
-  Login() {
-    console.log('auth result is: ' + this.service.loginWithCredentials(this.userName, this.password) + this.service.temp);
+  //
+  onSubmit(formValue) {
+    console.log(formValue);
+    console.log(formValue.login.userName + '---' + formValue.login.password + '\nauth result is: ' +
+      this.service.loginWithCredentials(formValue.login.userName, formValue.login.password));
   }
 
   ngOnInit() {
